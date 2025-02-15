7f702254a5a736117114f3e14c87fdcc06a4999a
Aravindhan <personalflareup@gmail.com>
2025-02-15 15:47:28 +0530

    Update: [2]

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
 set -o errexit
 
+# Make build.sh executable
+chmod a+x build.sh
+
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
